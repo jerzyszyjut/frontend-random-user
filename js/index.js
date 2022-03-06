3a16c20189ec5ed5329a6047d545f248c061de33
Jerzy Szyjut <kieszyk.pl@gmail.com>
2022-03-06 10:53:37 +0100

    Linted project

--- js/index.js
+++ js/index.js
@@ -1,82 +1,84 @@
 import TokenService from './services/history.service.js';
 import APIService from './services/api.service.js';
-import { hideAddressContainer, hideInfo, showUserContainer, displayUser } from './ui/index.js';
+import {
+  hideAddressContainer,
+  hideInfo,
+  showUserContainer,
+  displayUser,
+} from './ui/index.js';
 
 const parameters = ['name', 'picture', 'nat', 'registered', 'location'];
 
 let isAddressHidden = false;
 
 function parseLocationAddress(location) {
-    const stringLocation = `${location.street.number} ${location.street.name}, ${location.city} ${location.postcode}, ${location.state}, ${location.country}`;
-    return stringLocation;
+  const stringLocation = `${location.street.number} ${location.street.name}, ${location.city} ${location.postcode}, ${location.state}, ${location.country}`;
+  return stringLocation;
 }
 
 function parseUserForHistory(user) {
-    const parsedUser = {
-        first_name: user.name.first,
-        last_name: user.name.last,
-        country: user.nat,
-        register_date: user.registered.date
-    }
+  const parsedUser = {
+    first_name: user.name.first,
+    last_name: user.name.last,
+    country: user.nat,
+    register_date: user.registered.date,
+  };
 
-    return parsedUser;
+  return parsedUser;
 }
 
 function parseUserForDisplay(user) {
-    let locationAddress;
-    if (user.location) {
-        locationAddress = parseLocationAddress(user.location);
-    }
-    else {
-        locationAddress = 'Not fetched'
-    }
+  let locationAddress;
+  if (user.location) {
+    locationAddress = parseLocationAddress(user.location);
+  } else {
+    locationAddress = 'Not fetched';
+  }
 
-    const parsedUser = {
-        profile_picture: user.picture.large,
-        first_name: user.name.first,
-        last_name: user.name.last,
-        register_date: new Date(user.registered.date).toLocaleString('en-GB', { timeZone: 'UTC' }),
-        nationality: user.nat,
-        location_address: locationAddress,
-    }
+  const parsedUser = {
+    profile_picture: user.picture.large,
+    first_name: user.name.first,
+    last_name: user.name.last,
+    register_date: new Date(user.registered.date).toLocaleString('en-GB', { timeZone: 'UTC' }),
+    nationality: user.nat,
+    location_address: locationAddress,
+  };
 
-    return parsedUser;
+  return parsedUser;
 }
 
 function parseParamentersToQueryString() {
-    let query = '?inc=';
-    for (const parameter in parameters) {
-        query += parameters[parameter];
-        query += ','
-    }
-    return query;
+  let query = '?inc=';
+  for (const parameter in parameters) {
+    query += parameters[parameter];
+    query += ',';
+  }
+  return query;
 }
 
 function addUserToLocalStorage(user) {
-    user = parseUserForHistory(user);
-    TokenService.addUserToUsersHistory(user);
+  TokenService.addUserToUsersHistory(parseUserForHistory(user));
 }
 
 function hideInfoAndShowUserContent() {
-    hideInfo();
-    showUserContainer();
+  hideInfo();
+  showUserContainer();
 }
 
 export function hideAddress() {
-    isAddressHidden = !isAddressHidden;
+  isAddressHidden = !isAddressHidden;
 
-    hideAddressContainer(isAddressHidden);
-    if (isAddressHidden) {
-        parameters.pop();
-    }
-    else {
-        parameters.push('location');
-    }
+  hideAddressContainer(isAddressHidden);
+  if (isAddressHidden) {
+    parameters.pop();
+  } else {
+    parameters.push('location');
+  }
 }
 
 export async function generateUser() {
-    const user = await APIService.getUser(parseParamentersToQueryString());
-    addUserToLocalStorage(user);
-    hideInfoAndShowUserContent();
-    displayUser(parseUserForDisplay(user));
+  const user = await APIService.getUser(parseParamentersToQueryString());
+  addUserToLocalStorage(user);
+  hideInfoAndShowUserContent();
+  displayUser(parseUserForDisplay(user));
 }
--- js/ui/history.js
+++ js/ui/history.js
@@ -2,96 +2,100 @@ const outerTable = document.getElementById('history-table');
 const table = document.getElementById('table');
 
 function updateIcons(filters) {
-    const lastNameSortIcon = document.getElementById('last-name-sort-icon');
-    const registerNameSortIcon = document.getElementById('register-date-sort-icon');
-    switch (filters.lastName) {
-        case null:
-            lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort');
-            break;
-        case true:
-            lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort-up');
-            break;
-        case false:
-            lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort-down');
-            break;
-    }
-    switch (filters.registerDate) {
-        case null:
-            registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort');
-            break;
-        case true:
-            registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort-up');
-            break;
-        case false:
-            registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort-down');
-            break;
-    }
+  const lastNameSortIcon = document.getElementById('last-name-sort-icon');
+  const registerNameSortIcon = document.getElementById('register-date-sort-icon');
+  switch (filters.lastName) {
+    case null:
+      lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort');
+      break;
+    case true:
+      lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort-up');
+      break;
+    case false:
+      lastNameSortIcon.setAttribute('class', 'fa-solid fa-sort-down');
+      break;
+    default:
+      break;
+  }
+  switch (filters.registerDate) {
+    case null:
+      registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort');
+      break;
+    case true:
+      registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort-up');
+      break;
+    case false:
+      registerNameSortIcon.setAttribute('class', 'fa-solid fa-sort-down');
+      break;
+    default:
+      break;
+  }
 }
 
 function generateCell(className, value) {
-    const cellElement = document.createElement('div');
-    cellElement.setAttribute('class', `cell ${className}`);
-    if (className === 'register_date') {
-        value = new Date(value).toLocaleString('en-GB', { timeZone: 'UTC' })
-    }
-    cellElement.appendChild(document.createTextNode(value));
-    return cellElement;
+  const cellElement = document.createElement('div');
+  cellElement.setAttribute('class', `cell ${className}`);
+  let date;
+  if (className === 'register_date') {
+    date = new Date(value).toLocaleString('en-GB', { timeZone: 'UTC' });
+  }
+  cellElement.appendChild(document.createTextNode(date));
+  return cellElement;
 }
 
 function generateRow(object) {
-    const rowElement = document.createElement('div');
-    rowElement.setAttribute('class', 'row');
-    for (const key in object) {
-        const cell = generateCell(key, object[key]);
-        rowElement.appendChild(cell);
-    }
-    return rowElement;
+  const rowElement = document.createElement('div');
+  rowElement.setAttribute('class', 'row');
+  for (const key in object) {
+    const cell = generateCell(key, object[key]);
+    rowElement.appendChild(cell);
+  }
+  return rowElement;
 }
 
 function generateTable(users) {
-    const rows = document.createElement('div');
-    rows.setAttribute('class', 'rows');
+  const rows = document.createElement('div');
+  rows.setAttribute('class', 'rows');
 
-    for (const user in users) {
-        const rowElement = generateRow(users[user]);
-        rows.appendChild(rowElement);
-    }
-    return rows;
+  for (const user in users) {
+    const rowElement = generateRow(users[user]);
+    rows.appendChild(rowElement);
+  }
+  return rows;
 }
 
 function generateInformation() {
-    const infoElement = document.createElement('div');
-    infoElement.setAttribute('class', 'info');
-    const infoText = 'Nothing to see here yet. Generate users on home page!';
-    infoElement.appendChild(document.createTextNode(infoText)); return infoElement;
+  const infoElement = document.createElement('div');
+  infoElement.setAttribute('class', 'info');
+  const infoText = 'Nothing to see here yet. Generate users on home page!';
+  infoElement.appendChild(document.createTextNode(infoText)); return infoElement;
 }
 
 function removeChilds(parent) {
-    while (parent.lastChild) {
-        parent.removeChild(parent.lastChild);
-    }
+  while (parent.lastChild) {
+    parent.removeChild(parent.lastChild);
+  }
 }
 
 function generatehistoryTableContent(users) {
-    let content;
-    let type;
-    if (localStorage.length == 0) {
-        content = generateInformation();
-        type = 'info';
-    }
-    else {
-        content = generateTable(users);
-        type = 'table';
-    }
-    return { content, type };
+  let content;
+  let type;
+  if (localStorage.length === 0) {
+    content = generateInformation();
+    type = 'info';
+  } else {
+    content = generateTable(users);
+    type = 'table';
+  }
+  return { content, type };
 }
 
-export function displayHistoryContent(users, filters) {
-    removeChilds(table);
-    updateIcons(filters);
-    const { content, type } = generatehistoryTableContent(users);
-    if (type == 'info') {
-        outerTable.removeChild(outerTable.childNodes[1]);
-    }
-    table.appendChild(content);
+export default function displayHistoryContent(users, filters) {
+  removeChilds(table);
+  updateIcons(filters);
+  const { content, type } = generatehistoryTableContent(users);
+  if (type === 'info') {
+    outerTable.removeChild(outerTable.childNodes[1]);
+  }
+  table.appendChild(content);
 }
